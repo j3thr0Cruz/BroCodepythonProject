9933fa1c12472992a8082a5b71840883c0955658
j3thr0Cruz <j3thro.cruz@gmail.com>
2022-11-16 14:03:19 -0500

    brocode

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+#NESTED
+# rows = int(input("how many rows"))
+# column = int(input("how many columns"))
+# symbol = input("what symbol")
+#
+# for i in range(rows):
+#     for j in range(column):
+#         print(symbol, end = "")
+#     print()
+
+#break continue pass
+
+# while True:
+#     name = input("enter your name: ")
+#     if name != "":
+#         break
+
+# phone_number = "123-456-7890"
+#
+# for i in phone_number:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+for i in range(1,21):
+    if i == 13:
+         pass
+    else:
+        print(i)
